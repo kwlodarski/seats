83211bee9b296f388e1a3c7296421d7279e94ef4
Konrad Wlodarski <k.wlodarski@web-systems.pl>
2023-01-12 12:09:42 +0100

    Added mobile version and change layout colors

--- resources/js/calendar.vue
+++ resources/js/calendar.vue
@@ -1,63 +1,126 @@
 <template>
     <div class="relative pb-8">
-        <div class="flex justify-between my-6">
+        <div class="flex justify-between my-6 items-center" v-touch:swipe.left="nextMonth" v-touch:swipe.right="prevMonth">
             <button 
                 @click="prevMonth" 
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out"
-            >poprzedni miesiąc</button>
-            <span class="text-lg">{{ zeroPad(currentMonth, 10) }} {{ currentYear }}</span>
+                class="inline-block px-3 py-1.5 bg-k4pink text-white font-medium text-xs leading-tight uppercase rounded-3xl border-4 border-k4pink hover:bg-white hover:text-k4pink focus:outline-none focus:ring-0 transition duration-300 ease-in-out"
+            >
+                <svg class="w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+            </button>
+            <span class="text-lg font-bold">{{ zeroPad(currentMonth, 10) }} {{ currentYear }}</span>
             <button
                 @click="nextMonth"
                 type="button"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                class="inline-block px-6 py-2.5 bg-blue-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-600 active:shadow-lg transition duration-150 ease-in-out"
-            >następny miesiąc</button>
-        </div>
-        <table class="w-full">
-            <tr>
-            <td></td>
-            <td class="px-2 py-1 text-center" v-for="(employee, index) in employees" :key="index">
-                <input
-                    type="checkbox"
-                    :class="[`frequency-${day}`]"
-                    @change="changeAllFrequency($event, index, employee.id)"
-                />
-                {{ employee.name }}</td>
-            <td></td>
-            </tr>
-            <tr
-                v-for="{ day, index, freeDay, holiday, weekday} in currentMonthObject"
-                :key="index"
-                class="even:bg-gray-50 odd:bg-white border border-gray-200"
+                class="inline-block px-3 py-1.5 bg-k4pink text-white font-medium text-xs leading-tight uppercase rounded-3xl border-4 border-k4pink hover:bg-white hover:text-k4pink focus:outline-none focus:ring-0 transition duration-300 ease-in-out"
             >
-            <td class="px-2 py-2 text-center" :class="{ 'bg-red-300 text-black': freeDay, 'bg-green-300 text-black': isToday(day) }">{{ weekday }} {{ day }}.{{ zeroPad(currentMonth, 10) }}.{{ currentYear }}</td>
-            <template v-if="!freeDay">
-                <td v-for="(employee, employeeIndex) in employees" :key="employeeIndex" class="text-center px-2 py-1" :class="{ 'bg-red-300 text-black': freeDay, 'bg-green-300 text-black': isToday(day) }">
-                    <input
-                    v-if="!freeDay"
-                    type="checkbox"
-                    :class="[`frequency-${day}`]"
-                    @change="changeFrequency($event, index, day, employee.id)"
-                    :checked="currentMonthObject[index].frequency.includes(employee.id) ? true : false"
-                    />
-                </td>
-            </template>
-            <template v-else>
-                <td class="text-center px-2 py-1" :class="{ 'bg-red-300 text-black': freeDay, 'bg-green-300 text-white': isToday(day) }" :colspan="employees.length">
-                    {{ holiday }}
-                </td>
-            </template>
-            <td class="px-2 py-1 text-center" :class="{'bg-green-300 text-black': currentMonthObject[index].frequency.length < 7 && !freeDay || freeDay && isToday(day), 'bg-red-300 text-black': currentMonthObject[index].frequency.length > 7, 'bg-yellow-500': currentMonthObject[index].frequency.length === 7, 'bg-red-300': freeDay && !isToday(day)}">
-                <span v-if="!freeDay">{{
-                currentMonthObject[index].frequency.length
-                }}</span>
-            </td>
-            </tr>
-        </table>
+                <svg class="w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
+            </button>
+        </div>
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="w-full table-auto">
+                        <tr class="row">
+                            <template v-if="isMobile() && currentEmployeeIndex !== null">
+                                <td colspan="3" v-touch:swipe.left="nextEmployee" v-touch:swipe.right="prevEmployee">
+                                    <div class="flex w-full justify-between items-center">
+                                        <div class="flex-1">
+                                            <button v-if="currentEmployeeIndex !== 0" class="inline-block px-2 py-1.5 bg-k4pink text-white font-medium text-xs leading-tight uppercase rounded-3xl border-4 border-k4pink hover:bg-white hover:text-k4pink focus:outline-none focus:ring-0 transition duration-300 ease-in-out" @click="prevEmployee" >
+                                                <svg class="w-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                                            </button>
+                                        </div>
+                                        <div class="flex-2 text-center">
+                                            <p class="font-bold mb-0">{{ employees[currentEmployeeIndex].name }}</p>
+                                            <div class="form-check flex justify-center items-center">
+                                                <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-k4green checked:border-k4green focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" @change="changeAllFrequency($event, index, employees[currentEmployeeIndex].id)" :id="[`employee-${currentEmployeeIndex}`]" :checked="isFullFrequency(employees[currentEmployeeIndex].id) ? true : false">
+                                                <label class="form-check-label inline-block text-sm font-light" :for="[`employee-${currentEmployeeIndex}`]">
+                                                    wszystkie dni
+                                                </label>
+                                            </div>                                            
+                                        </div>
+                                        <div class="flex-1 text-right">
+                                            <button v-if="currentEmployeeIndex < employees.length - 1" class="inline-block px-2 py-1.5 bg-k4pink text-white font-medium text-xs leading-tight uppercase rounded-3xl border-4 border-k4pink hover:bg-white hover:text-k4pink focus:outline-none focus:ring-0 transition duration-300 ease-in-out" @click="nextEmployee">
+                                                <svg class="w-2 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </td>
+                            </template>
+                            <template v-else>
+                                <td></td>
+                                <td class="px-2 py-1 text-center cell-for-slider" v-for="(employee, index) in employees" :key="index">
+                                    <p class="font-bold mb-0">{{ employees[index].name }}</p>
+                                    <div class="form-check flex justify-center items-center">
+                                        <input class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-k4green checked:border-k4green focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="checkbox" @change="changeAllFrequency($event, index, employee.id)" :id="[`employee-${index}`]" :checked="isFullFrequency(employees[index].id) ? true : false">
+                                        <label class="form-check-label inline-block text-sm font-light" :for="[`employee-${index}`]">
+                                            wszystkie dni
+                                        </label>
+                                    </div>
+                                </td>
+                                <td></td>
+                            </template>
+                        </tr>
+                        <tr
+                            v-for="{ day, index, freeDay, holiday, weekday} in currentMonthObject"
+                            :key="index"
+                            class="even:bg-gray-50 odd:bg-white border border-gray-200 row"
+                        >
+                            <template v-if="isMobile() && currentEmployeeIndex !== null">
+                                <td class="px-2 py-2 text-center w-28" :class="{ 'bg-k4gray text-white': freeDay, 'bg-k4green text-white': isToday(day) }">{{ weekday }} {{ day }}.{{ zeroPad(currentMonth, 10) }}</td>
+                                <template v-if="!freeDay">
+                                    <td class="text-center px-2 py-1" :class="{ 'bg-k4gray text-white': freeDay, 'bg-k4green text-white': isToday(day) }" v-touch:swipe.left="nextEmployee" v-touch:swipe.right="prevEmployee">
+                                        <input 
+                                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-k4green checked:border-k4green focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                                            v-if="!freeDay"
+                                            type="checkbox"
+                                            :class="[`frequency-${day}`],{'checked:bg-k4green checked:border-k4green': !isToday(day), 'checked:bg-k4pink checked:border-k4pink': isToday(day)}"
+                                            @change="changeFrequency($event, index, day, employees[currentEmployeeIndex].id)"
+                                            :checked="currentMonthObject[index].frequency.includes(employees[currentEmployeeIndex].id) ? true : false"
+                                        />
+                                    </td>
+                                </template>
+                                <template v-else>
+                                    <td class="text-center px-2 py-1" :class="{ 'bg-k4gray text-white': freeDay, 'bg-k4green text-white': isToday(day) }" v-touch:swipe.left="nextEmployee" v-touch:swipe.right="prevEmployee">
+                                        {{ holiday }}
+                                    </td>
+                                </template>
+                            </template>
+                            <template v-else>
+                                <td class="px-2 py-2 text-center" :class="{ 'bg-k4gray text-white': freeDay, 'bg-k4green text-white': isToday(day) }">{{ weekday }} {{ day }}.{{ zeroPad(currentMonth, 10) }}.{{ currentYear }}</td>
+                                <template v-if="!freeDay">
+                                    <td v-for="(employee, employeeIndex) in employees" :key="employeeIndex" class="text-center px-2 py-1" :class="{ 'bg-k4gray text-white': freeDay, 'bg-k4green text-white': isToday(day) }">
+                                        <input
+                                            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white focus:outline-none transition duration-200 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                                            v-if="!freeDay"
+                                            type="checkbox"
+                                            :class="[`frequency-${day}`],{'checked:bg-k4green checked:border-k4green': !isToday(day), 'checked:bg-k4pink checked:border-k4pink': isToday(day)}"
+                                            @change="changeFrequency($event, index, day, employee.id)"
+                                            :checked="currentMonthObject[index].frequency.includes(employee.id) ? true : false"
+                                        />
+                                    </td>
+                                </template>
+                                <template v-else>
+                                    <td class="text-center px-2 py-1" :class="{ 'bg-k4gray text-white': freeDay, 'bg-k4green text-white': isToday(day) }" :colspan="employees.length">
+                                        {{ holiday }}
+                                    </td>
+                                </template>
+                            </template>
+                            <td class="px-2 py-1 text-center w-8" :class="{'bg-k4green text-white': currentMonthObject[index].frequency.length < 7 && !freeDay || freeDay && isToday(day), 'bg-k4gray text-white': currentMonthObject[index].frequency.length > 7, 'bg-k4yellow': currentMonthObject[index].frequency.length === 7, 'bg-k4gray': freeDay && !isToday(day)}">
+                                <span v-if="!freeDay">{{
+                                currentMonthObject[index].frequency.length
+                                }}</span>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
         <div v-if="loading" class="w-full h-full absolute top-0 left-0 bg-gray-100/70 grid content-center justify-center">
             <div class="spinner-grow inline-block w-12 h-12 bg-current rounded-full opacity-0" role="status">
                 <span class="visually-hidden">Loading...</span>
@@ -76,6 +139,7 @@ export default {
         const currentMonth = today.getMonth() + 1;
         const currentYear = today.getFullYear();
         let currentMonthObject = [];
+        let currentEmployeeIndex = null;
         const monthDayAmount = new Date(currentYear, currentMonth, 0).getDate();
         for (let i = 0; i < monthDayAmount; i++) {
             currentMonthObject = [
@@ -97,11 +161,43 @@ export default {
             currentYear: currentYear,
             currentMonthObject: currentMonthObject,
             holidays: [],
+            currentEmployeeIndex: currentEmployeeIndex,
             loading: true,
             weekday: ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'Sb']
         };
     },
     methods: {
+        isFullFrequency(employeeId) {
+            let employeeFrequency = [];
+            const onlyWorkDays = this.currentMonthObject.filter(function (day) {
+                return day.freeDay === false;
+            });
+            onlyWorkDays.every(day => { 
+                if (day.frequency.includes(employeeId)) {
+                    employeeFrequency = [...employeeFrequency, employeeId];
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+            if (onlyWorkDays.length === employeeFrequency.length) {
+                return true;  
+            } else {
+                return false;
+            }
+        },
+        prevEmployee() {
+            if (this.currentEmployeeIndex > 0) {
+                this.currentEmployeeIndex--;
+            }
+            return true;
+        },
+        nextEmployee() {
+            if (this.currentEmployeeIndex < this.employees.length - 1) {
+                this.currentEmployeeIndex++;
+            }
+            return true;
+        },
         zeroPad(nr, base){
             const len = (String(base).length - String(nr).length)+1;
             return len > 0? new Array(len).join('0')+nr : nr;
@@ -329,6 +425,9 @@ export default {
                 })
                 .then(function(data) {
                     self.employees = data.employees;
+                    if(self.employees.length > 0 && self.currentEmployeeIndex === null) {
+                        self.currentEmployeeIndex = 0;
+                    }
                 })
         },
         getHolidays() {
@@ -347,6 +446,12 @@ export default {
             }).catch(function (error) {
                 console.error(error);
             });
+        },
+        isMobile() {
+            if (this.$vssWidth > 1024) {
+                return false;
+            }
+            return true;
         }
     },
     mounted() {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Styles -->
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
     
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
@@ -25,35 +18,55 @@
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none">
     <div id="app">
-        <header class="bg-blue-900 py-6">
-            <div class="container mx-auto flex justify-between items-center px-6">
-                <div>
-                    <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                        {{ config('app.name', 'Laravel') }}
+        <header>
+            <nav class="bg-k4dark border-gray-200 px-2 sm:px-4 py-2.5">
+                <div class="container flex flex-wrap items-center justify-between mx-auto">
+                    <a href="{{ url('/') }}" class="flex items-center">
+                        <svg class="w-16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 750 750" xmlSpace="preserve" class="QYCgpAJfeMCVB1x7xzz9 QubB07L3HSo1mvsypeLs xi3AllYKY8dOlOc5nm7K">
+                            <g>
+                                <path class="fill-k4pink" d="M29.1,177.8h68v141.7l106.6-141.7h85.2L139.5,374.8l151.3,198h-85.9L97,430.1v88.2H29.1V177.8z"></path>
+                                <path class="fill-k4pink" d="M455.3,428.6V178.1h-82L224.2,375.3l90.9,120.6l72.9-0.2v76.9h67.3v-76.7h20.4V463h15.7v-34.4H455.3zM388,428.6H266.8l121.2-160V428.6z"></path>
+                                <rect x="490.3" y="478.4" class="fill-k4green" width="33.5" height="33.4"></rect>
+                                <path class="fill-white" d="M691.6,328.4h35v193.2h-35V328.4z"></path>
+                                <path class="fill-white" d="M648.9,399.3c-12.7-13.7-28.2-20.6-46.4-20.6c-8.4,0-16.2,1.6-23.6,4.7c-7.3,3.2-14.2,7.9-20.5,14.2v-15.4
+                                h-34.7V463h15.1v63.2h-15.1v46.4h34.7v-65.7c6.8,6.5,13.7,11.2,20.6,14s14.4,4.3,22.4,4.3c18,0,33.6-7,46.8-20.9
+                                c13.1-14,19.7-31.3,19.7-52.1C668,430.7,661.6,413,648.9,399.3z M622.2,481.5c-7.3,7.8-16.3,11.7-27,11.7c-11,0-20.2-3.8-27.4-11.5
+                                c-7.3-7.6-10.9-17.7-10.9-30c0-12.1,3.6-21.9,10.9-29.5c7.3-7.5,16.4-11.3,27.4-11.3c10.9,0,20,3.8,27.2,11.5
+                                c7.2,7.6,10.8,17.4,10.8,29.4C633.1,463.8,629.5,473.8,622.2,481.5z"></path>
+                            </g>
+                        </svg>
                     </a>
+                    <button id="open-mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-white md:hidden focus:outline-none focus:ring-0" aria-controls="navbar-default" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <svg class="w-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    </button>
+                    <div class="hidden fixed md:static w-3/4 h-full top-0 right-0 bg-k4dark z-50 md:block md:w-auto" id="navbar-default">
+                        <button id="close-mobile-menu" type="button" class="absolute right-6 py-2 px-3 top-6 text-white md:hidden focus:outline-none focus:ring-0">
+                            <span class="sr-only">Close main menu</span>
+                            <svg class="w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg>
+                        </button>
+                        <ul class="flex flex-col p-4 mt-12 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+                            @guest
+                                <li><a class="block py-4 md:py-2 pl-3 pr-4 text-white md:p-0 text-xs tracking-widest font-bold uppercase" href="{{ url('/') }}">{{ __('Kalendarz') }}</a></li>
+                                <li><a class="block py-4 md:py-2 pl-3 pr-4 text-white md:p-0 text-xs tracking-widest font-bold uppercase" href="{{ route('login') }}">{{ __('Logowanie') }}</a></li>
+                                @if (Route::has('register'))
+                                    <li><a class="block py-4 md:py-2 pl-3 pr-4 text-white md:p-0 text-xs tracking-widest font-bold uppercase" href="{{ route('register') }}">{{ __('Rejestracja') }}</a></li>
+                                @endif
+                            @else
+                                {{-- <span>{{ Auth::user()->name }}</span> --}}
+                                <li><a class="block py-4 md:py-2 pl-3 pr-4 text-white md:p-0 text-xs tracking-widest font-bold uppercase" href="{{ url('/') }}">{{ __('Kalendarz') }}</a></li>
+                                <li><a class="block py-4 md:py-2 pl-3 pr-4 text-white md:p-0 text-xs tracking-widest font-bold uppercase" href="{{ route('settings') }}">{{ __('Ustawienia') }}</a></li>
+                                <li>
+                                    <a href="{{ route('logout') }}" class="block py-4 md:py-2 pl-3 pr-4 text-white md:p-0 text-xs tracking-widest font-bold uppercase" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Wyloguj') }}</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
                 </div>
-                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
-                    @guest
-                        <a class="no-underline hover:underline" href="{{ url('/') }}">{{ __('Kalendarz') }}</a>
-                        <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Logowanie') }}</a>
-                        @if (Route::has('register'))
-                            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Rejestracja') }}</a>
-                        @endif
-                    @else
-                        {{-- <span>{{ Auth::user()->name }}</span> --}}
-                        <a class="no-underline hover:underline" href="{{ url('/') }}">{{ __('Kalendarz') }}</a>
-                        <a class="no-underline hover:underline" href="{{ route('settings') }}">{{ __('Ustawienia') }}</a>
-
-                        <a href="{{ route('logout') }}"
-                           class="no-underline hover:underline"
-                           onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">{{ __('Wyloguj') }}</a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
-                            {{ csrf_field() }}
-                        </form>
-                    @endguest
-                </nav>
-            </div>
+            </nav>
         </header>
 
         @yield('content')
