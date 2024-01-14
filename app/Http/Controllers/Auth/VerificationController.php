ceed7b94494d011aac9e457c434cb9761e541a20
Konrad Wlodarski <k.wlodarski@web-systems.pl>
2024-01-14 11:42:10 +0100

    Routing bug fix

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::SETTINGS;
+    protected $redirectTo = RouteServiceProvider::DASHBOARD;
 
     /**
      * Create a new controller instance.
